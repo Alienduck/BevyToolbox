a011a0fd62d4be7577f555538172fe1d46665ac5
Owen Neuillyr <jacquonneries@gmail.com>
2026-05-21 08:44:31 +0200

    create simple example of tween service

--- examples/tween_service.rs
+++ examples/tween_service.rs
@@ -1,21 +1,19 @@
 use bevy::prelude::*;
 use bevy_toolbox::tools_3d::{
-    tween_service::{Tween, TweenFloat, TweenInfo, TweenPlugin},
+    tween_service::{TweenAppExt, TweenInfo, TweenPlugin, TweenService},
     utils::{EasingDirection, EasingStyle},
 };
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, TweenPlugin))
+        .register_tween::<Sprite>()
         .add_systems(Startup, setup)
-        .add_systems(Update, sync_transform)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
-    let start = TweenFloat(-300.0);
-    let goal = TweenFloat(300.0);
     let info = TweenInfo {
         duration: 2.0,
         easing_style: EasingStyle::Linear,
@@ -31,13 +29,17 @@ fn setup(mut commands: Commands) {
             ..default()
         },
         Transform::from_xyz(-300.0, 0.0, 0.0),
-        start,
-        Tween::new(start, goal, info),
+        TweenService::create(
+            info,
+            Transform::from_xyz(300.0, 100.0, 0.0).with_scale(Vec3::splat(2.0)),
+        ),
+        TweenService::create(
+            info,
+            Sprite {
+                color: Color::srgb(0.2, 1.0, 0.2),
+                custom_size: Some(Vec2::new(100.0, 100.0)),
+                ..default()
+            },
+        ),
     ));
-}
-
-fn sync_transform(mut query: Query<(&TweenFloat, &mut Transform)>) {
-    for (val, mut transform) in query.iter_mut() {
-        transform.translation.x = val.0;
-    }
 }
