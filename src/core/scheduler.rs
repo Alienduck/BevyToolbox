4d1f137f11c2e2d0120854d87cd0bac4421d34af
Owen Neuillyr <jacquonneries@gmail.com>
2026-05-29 18:56:11 +0200

    scheduler spread time by resuming

--- src/core/scheduler.rs
+++ src/core/scheduler.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use mlua::{BorrowedStr, Function, Lua, MultiValue, Thread, ThreadStatus, Value};
+use mlua::{MultiValue, Thread, ThreadStatus, Value};
 
 struct ScheduledThread {
     thread: Thread,
@@ -25,37 +25,6 @@ impl LuaScheduler {
     }
 }
 
-pub fn register_task(lua: &Lua, scheduler: &mut LuaScheduler) {
-    let task_table = lua.create_table().unwrap();
-
-    task_table
-        .set(
-            "wait",
-            lua.create_function(|_, t: Option<f64>| {
-                let yield_fn: Function = mlua::Lua::new()
-                    .globals()
-                    .get("coroutine")
-                    .and_then(|t: mlua::Table| t.get("yield"))
-                    .unwrap_or_else(|_| panic!("coroutine.yield not found"));
-                Ok(())
-            })
-            .unwrap(),
-        )
-        .unwrap();
-
-    let wait_fn = lua
-        .create_function(|lua, t: Option<f64>| {
-            let coroutine: mlua::Table = lua.globals().get("coroutine")?;
-            let yield_fn: Function = coroutine.get("yield")?;
-            yield_fn.call::<MultiValue>(("wait", t.unwrap_or(0.0)))?;
-            Ok(())
-        })
-        .unwrap();
-    task_table.set("wait", wait_fn).unwrap();
-
-    lua.globals().set("task", task_table).unwrap();
-}
-
 pub fn tick_scheduler(mut scheduler: NonSendMut<LuaScheduler>, time: Res<Time>) {
     let elapsed = time.elapsed_secs_f64();
     let mut i = 0;
@@ -69,18 +38,17 @@ pub fn tick_scheduler(mut scheduler: NonSendMut<LuaScheduler>, time: Res<Time>) 
         match scheduler.threads[i].thread.status() {
             ThreadStatus::Resumable => match scheduler.threads[i].thread.resume::<MultiValue>(()) {
                 Ok(values) => {
-                    let mut iter = values.iter();
-                    let is_wait = matches!(
-                        iter.next(),
-                        Some(Value::String(s)) if s.to_str().unwrap() == "wait"
-                    );
-
-                    if is_wait {
-                        let wait_secs = iter.next().and_then(|v| v.as_f64()).unwrap_or(0.0);
-                        scheduler.threads[i].resume_at = elapsed + wait_secs;
-                        i += 1;
-                    } else {
-                        i += 1;
+                    let mut iter = values.into_iter();
+                    match iter.next() {
+                        Some(Value::String(s)) if s.to_str().unwrap() == "wait" => {
+                            let secs = iter
+                                .next()
+                                .and_then(|v| v.as_f64().or_else(|| v.as_i64().map(|i| i as f64)))
+                                .unwrap_or(0.0);
+                            scheduler.threads[i].resume_at = elapsed + secs;
+                            i += 1;
+                        }
+                        _ => i += 1,
                     }
                 }
                 Err(e) => {
@@ -88,7 +56,7 @@ pub fn tick_scheduler(mut scheduler: NonSendMut<LuaScheduler>, time: Res<Time>) 
                     scheduler.threads.remove(i);
                 }
             },
-            ThreadStatus::Error => {
+            ThreadStatus::Finished | ThreadStatus::Error => {
                 scheduler.threads.remove(i);
             }
             _ => {
